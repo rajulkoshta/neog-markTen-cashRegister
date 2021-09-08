3a857256b9ee8cd9d95243aefadef90f6b0c6f4e
“Rajul” <rajulkoshta10@gmail.com>
2021-09-08 20:09:14 +0530

    chnaged files

---
+++ index.js
@@ -0,0 +1,10 @@
+let billInput=document.querySelector("#billAmount");
+let cashInput =document.querySelector("#cashAmount");
+let checkBtn = document.querySelector(".btn-primary");
+
+
+
+checkBtn.addEventListener("click",clickHandler);
+function clickHandler() {
+    alert("clicked");
+}
